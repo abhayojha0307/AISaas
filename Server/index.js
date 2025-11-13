4b22c40cdc256730f4604b527475f361413162dc
abhayojha0307 <abhayojha0307@gmail.com>
2025-11-14 03:06:01 +0530

    Server:Feat: basic server setup

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the AISaas Server!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
